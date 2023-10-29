a2ae9600c0ef3dc08c306a7ae170e9a5d12b2513
AlexTheGreat <alexandrewilbur@icloud.com>
2023-10-29 14:39:39 -0400

    Commit 29 Oct 2023

--- main.py
+++ main.py
@@ -5,34 +5,36 @@ import time
 from colorama import Fore
 
 colorama.init(autoreset=True)
-
 healthpoints = 20
+monsterhealth = 1
 
-print("Vous vous trouvez dans un couloir avec une porte de sortie avec un monstre SUPER effrayant!!!\n")
 while healthpoints >= 1:
-    monsterhealth = 1
-    print("Vous avez", Fore.GREEN + str(healthpoints), "point(s) de vie")
-    print("Le monstre a", Fore.RED + str(monsterhealth), "point(s) de vie")
+    print("Vous vous trouvez dans un couloir avec une porte de sortie avec un monstre SUPER effrayant!!!\n")
+    print("Vous avez", Fore.GREEN + str(healthpoints), "point(s) de vie\nLe monstre a", Fore.RED + str(monsterhealth), "point(s) de vie")
     print("Vous pouvez:\n")
     print(Fore.RED + "Combattre le monstre - 1\n")
     print(Fore.BLUE + "Contourner le monstre et aller ouvrir une autre porte - 2\n")
     fightchoice = str(input(Fore.WHITE + "Que voulez-vous faire?\n"))
 
+    #Condition pour l'action de se battre
+
     if fightchoice == "1":
         print("Vous roulez les dés pour battre le monstre!!! \n")
         numberonthedice = random.randint(1,6)
-        if numberonthedice <= monsterhealth:
+        if numberonthedice <= 4:
             print("Vous avez roulé un", numberonthedice, "\n, donc vous allez perdre un nombre de points de vie équivalent au nombre de points de vie du monstre (", monsterhealth, ")")
             healthpoints = healthpoints - monsterhealth
-        elif numberonthedice > monsterhealth:
+        elif numberonthedice > 1:
             print("Vous avez roulé un", numberonthedice, "\n, donc vous avez battu le montre qui avait", monsterhealth, "points de vie")
 
+    #Condition pour l'action de contourner
+
     elif fightchoice == "2":
-        print("Vous contourner le monstre pour passer dans la porte. \n")
+        print("Vous contourner le monstre pour passer dans la porte. \nVous perdez 1 point de vie en consequence")
         healthpoints = healthpoints - 1
-    monsterhealth =+ 1
+    monsterhealth = monsterhealth + 1
     if healthpoints >= 1:
-        print("Vouz allez dans la prochaine salle")
-    time.sleep(3)
-    
+        print("Vouz allez dans la prochaine salle\n\n")
+    time.sleep(5)
+
 print("Vous etes mort...")
